fef860e56da4617811f9266a40de90951e296c1e
Austin Booth <austinbooth@yahoo.co.uk>
2020-09-14 14:41:21 +0100

    added test data

---
+++ db/data/test-data/comments.js
@@ -0,0 +1,62 @@
+module.exports = [
+  {
+    time_stamp: "1586549847",
+    body: "body for comment 1 for experience 1",
+    likes: "1",
+    username: "user_b",
+    belongs_to_title: "experience 1",
+  },
+  {
+    time_stamp: "1594549847",
+    body: "body for comment 2 for experience 1",
+    likes: "5",
+    username: "user_c",
+    belongs_to_title: "experience 1",
+  },
+
+  {
+    time_stamp: "1574549847",
+    body: "body for comment 1 for experience 2",
+    likes: "1",
+    username: "user_a",
+    belongs_to_title: "experience 2",
+  },
+
+  {
+    time_stamp: "1578858463",
+    body: "body for comment 1 for experience 3",
+    likes: "1",
+    username: "user_a",
+    belongs_to_title: "experience 1",
+  },
+  {
+    time_stamp: "1582858463",
+    body: "body for comment 2 for experience 3",
+    likes: "5",
+    username: "user_b",
+    belongs_to_title: "experience 3",
+  },
+
+  {
+    time_stamp: "1576889475",
+    body: "body for comment 1 for experience 4",
+    likes: "1",
+    username: "user_b",
+    belongs_to_title: "experience 4",
+  },
+
+  {
+    time_stamp: "1586866595",
+    body: "body for comment 1 for experience 5",
+    likes: "1",
+    username: "user_b",
+    belongs_to_title: "experience 4",
+  },
+  {
+    time_stamp: "1589886795",
+    body: "body for comment 1 for experience 5",
+    likes: "1",
+    username: "user_b",
+    belongs_to_title: "experience 4",
+  },
+];
