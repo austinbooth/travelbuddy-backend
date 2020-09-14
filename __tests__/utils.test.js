9ceed74d591e2f7a09b266a7d16a054a3aba5c51
Austin Booth <austinbooth@yahoo.co.uk>
2020-09-14 16:04:31 +0100

    formats dates

--- __tests__/utils.test.js
+++ __tests__/utils.test.js
@@ -1,5 +1,56 @@
 const { formatDates } = require("../db/utils/utils");
 
 describe("formatDates", () => {
-  //
+  test("returns a new array and object", () => {
+    const created_at = 0;
+    const input = [];
+    const inputObj = { created_at };
+
+    const output = formatDates(input);
+    expect(output).not.toBe(input);
+    expect(output[0]).not.toBe(inputObj);
+  });
+  test("works for a single object", () => {
+    const created_at = 0;
+    const input = [];
+    const inputObj = { created_at };
+    input[0] = inputObj;
+
+    const expectedOutput = [{ created_at: new Date(created_at) }];
+    const output = formatDates(input);
+    expect(output).toEqual(expectedOutput);
+  });
+  test("works for multiple objects", () => {
+    const created_at0 = 86400000000;
+    const created_at1 = 1600094700000;
+    const input = [{ created_at: created_at0 }, { created_at: created_at1 }];
+    const expectedDate0 = new Date(created_at0);
+    const expectedDate1 = new Date(created_at1);
+    const output = formatDates(input);
+    console.log(expectedDate0, expectedDate1, Date.now());
+    expect(output.length).toBe(2);
+    expect(output[0].created_at).toEqual(expectedDate0);
+    expect(output[1].created_at).toEqual(expectedDate1);
+  });
+  test("keeps all other object properties", () => {
+    const created_at = 946684800000;
+    const input = [
+      {
+        title: "experience title",
+        body: "experience body",
+        likes: 5,
+        created_at,
+      },
+    ];
+    const expectedOutput = [
+      {
+        title: "experience title",
+        body: "experience body",
+        likes: 5,
+        created_at: new Date(created_at),
+      },
+    ];
+    const output = formatDates(input);
+    expect(output).toEqual(expectedOutput);
+  });
 });
--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,3 +1,9 @@
 exports.formatDates = (arr) => {
-  //
+  const reformattedArr = arr.map((obj) => {
+    const objCopy = { ...obj };
+    const psqlTimestamp = new Date(objCopy.created_at);
+    objCopy.created_at = psqlTimestamp;
+    return objCopy;
+  });
+  return reformattedArr;
 }
