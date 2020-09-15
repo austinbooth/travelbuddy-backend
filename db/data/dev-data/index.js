e0fb1fc73cd6aa872e78635c93e2111ddd49fec2
Austin Booth <austinbooth@yahoo.co.uk>
2020-09-15 18:48:28 +0100

    adds tag-experience data for junction table

--- db/data/dev-data/index.js
+++ db/data/dev-data/index.js
@@ -3,3 +3,4 @@ exports.experienceData = require("./experiences");
 exports.imageData = require("./images");
 exports.tagData = require("./tags");
 exports.userData = require("./user");
+exports.tagExperienceJunctionData = require("./tag-experience-junctions");
