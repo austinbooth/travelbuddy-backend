c936f7137093b8bab8df2780e2ffcf6caa8945bf
Austin Booth <austinbooth@yahoo.co.uk>
2020-09-15 21:52:37 +0100

    changes references_image to image_URL

--- db/data/dev-data/images.js
+++ db/data/dev-data/images.js
@@ -1,88 +1,105 @@
-module.exports = [{
-    image_desc: "a street performer raises his hat as he addresses his audience",
+module.exports = [
+  {
+    image_desc:
+      "a street performer raises his hat as he addresses his audience",
     belongs_to_title: "Fab place to watch street performers",
-    references_image: "streetPerformer.jpg",
+    image_URL: "streetPerformer.jpg",
   },
   {
     image_desc: "two customers stand in front of a stall piled with salami",
     belongs_to_title: "Really cute craft market.",
-    references_image: "marketStall.jpg",
+    image_URL: "marketStall.jpg",
   },
   {
     image_desc: "tourists stand listening intently to a speaker out of shot",
     belongs_to_title: "Free history lesson outside the Yorvik centre.",
-    references_image: "historyQueue.jpg",
+    image_URL: "historyQueue.jpg",
   },
   {
-    image_desc: "a DJ wearing headphones has her back to us as she uses her decs to mix music",
+    image_desc:
+      "a DJ wearing headphones has her back to us as she uses her decs to mix music",
     belongs_to_title: "Murder at the silent disco!",
-    references_image: "silentDisco.jpg",
+    image_URL: "silentDisco.jpg",
   },
   {
-    image_desc: "a girl looks at her phone screen which displays another girl standing behind directing her phone at the back of the first girl. They are surrounded by trees and autumn leaves",
+    image_desc:
+      "a girl looks at her phone screen which displays another girl standing behind directing her phone at the back of the first girl. They are surrounded by trees and autumn leaves",
     belongs_to_title: "Great place for photographs",
-    references_image: "parkSelfies.jpg",
+    image_URL: "parkSelfies.jpg",
   },
   {
-    image_desc: "a hand deposits wood into a fire burning in a metal container. Sounding is a forest setting",
+    image_desc:
+      "a hand deposits wood into a fire burning in a metal container. Sounding is a forest setting",
     belongs_to_title: "Free outdoor cooking.",
-    references_image: "firePit.jpg",
+    image_URL: "firePit.jpg",
   },
   {
-    image_desc: "a ginger and white cat sits upon a grave-stone as a hand reaches to scratch the cat's head",
+    image_desc:
+      "a ginger and white cat sits upon a grave-stone as a hand reaches to scratch the cat's head",
     belongs_to_title: "Cat heaven!",
-    references_image: "minsterCat.jpg",
+    image_URL: "minsterCat.jpg",
   },
   {
-    image_desc: "in the foreground stand a number of used spray paint can of various colours. In the background a man, wearing a breathing mask, applies paint from a spray can onto a bright blue canvas",
+    image_desc:
+      "in the foreground stand a number of used spray paint can of various colours. In the background a man, wearing a breathing mask, applies paint from a spray can onto a bright blue canvas",
     belongs_to_title: "Graffiti workshop",
-    references_image: "graffitiWorkshop01.jpg",
+    image_URL: "graffitiWorkshop01.jpg",
   },
   {
-    image_desc: "a man, wearing a breathing mask, is spraying paint onto a painting of a bright yellow flower",
+    image_desc:
+      "a man, wearing a breathing mask, is spraying paint onto a painting of a bright yellow flower",
     belongs_to_title: "Graffiti workshop",
-    references_image: "graffitiWorkshop02.jpg",
+    image_URL: "graffitiWorkshop02.jpg",
   },
 
   //PARIS
 
   {
-    image_desc: "Pont de Bir-hakeim bridge lit with lights sits in front of the paris skyline at dusk",
+    image_desc:
+      "Pont de Bir-hakeim bridge lit with lights sits in front of the paris skyline at dusk",
     belongs_to_title: "WAY COOL BRIDGE",
-    references_image: "bridgeAtNight.jpg",
+    image_URL: "bridgeAtNight.jpg",
   },
   {
-    image_desc: "a wide angle view across a park, showing trees, grass and a number of people lying alone or in small groups on the grass",
+    image_desc:
+      "a wide angle view across a park, showing trees, grass and a number of people lying alone or in small groups on the grass",
     belongs_to_title: "Best place for a picnic",
-    references_image: "parisPicnic.jpg",
+    image_URL: "parisPicnic.jpg",
   },
   {
-    image_desc: "six bottles of wine stand in a group, each with a small label with price attached. In the background is a blurred image of shelves filled with bottles of wine. ",
+    image_desc:
+      "six bottles of wine stand in a group, each with a small label with price attached. In the background is a blurred image of shelves filled with bottles of wine. ",
     belongs_to_title: "wine tasting in an underground cellar",
-    references_image: "wineShop.jpg",
+    image_URL: "wineShop.jpg",
   },
   {
-    image_desc: "view down the path at a grave yard with tombs and statues lining either side",
+    image_desc:
+      "view down the path at a grave yard with tombs and statues lining either side",
     belongs_to_title: "hang out with the dead in style",
-    references_image: "parisGraves01.jpg",
-  }, {
-    image_desc: "a small statue of an angle sits atop a grave, as the dappled light peaks through the tress to illuminate a small metal plaque baring an inscription",
-    belongs_to_title: "hang out with the dead in style",
-    references_image: "parisGraves02.jpg",
+    image_URL: "parisGraves01.jpg",
   },
   {
-    image_desc: "a view showing a medium close up of the outside of the Pompidou Centre. The building is enveloped in external clear tubes and metal scaffolding",
+    image_desc:
+      "a small statue of an angle sits atop a grave, as the dappled light peaks through the tress to illuminate a small metal plaque baring an inscription",
+    belongs_to_title: "hang out with the dead in style",
+    image_URL: "parisGraves02.jpg",
+  },
+  {
+    image_desc:
+      "a view showing a medium close up of the outside of the Pompidou Centre. The building is enveloped in external clear tubes and metal scaffolding",
     belongs_to_title: "people watching in style",
-    references_image: "pompidouCentre.jpg",
+    image_URL: "pompidouCentre.jpg",
   },
   {
-    image_desc: "a medium shot of a stone wall, upon which are three repeated images of mickey-mouse's head (in the first image half his head coloured in blue, then the second white, then finally, in the third image, red, in replica of the French flag) imposed upon each of the mickey images is that of a black and white a small girl seeming to screen print the mickey heads onto the wall. ",
+    image_desc:
+      "a medium shot of a stone wall, upon which are three repeated images of mickey-mouse's head (in the first image half his head coloured in blue, then the second white, then finally, in the third image, red, in replica of the French flag) imposed upon each of the mickey images is that of a black and white a small girl seeming to screen print the mickey heads onto the wall. ",
     belongs_to_title: "cool street art",
-    references_image: "parisStreetArt.jpg",
+    image_URL: "parisStreetArt.jpg",
   },
   {
-    image_desc: "view of the internal courtyard at the Louvre Gallery, showing pale sand stone buildings surrounding a large courtyard of pale stone wide paths",
+    image_desc:
+      "view of the internal courtyard at the Louvre Gallery, showing pale sand stone buildings surrounding a large courtyard of pale stone wide paths",
     belongs_to_title: "luxury lunch like a Queen",
-    references_image: "louvreCourtyard.jpg",
+    image_URL: "louvreCourtyard.jpg",
   },
 ];
--- db/data/test-data/images.js
+++ db/data/test-data/images.js
@@ -2,26 +2,26 @@ module.exports = [
   {
     image_desc: "image 1 for experience 1",
     belongs_to_title: "experience 1",
-    references_image: "",
+    image_URL: "",
   },
   {
     image_desc: "image 1 for experience 2",
     belongs_to_title: "experience 2",
-    references_image: "",
+    image_URL: "",
   },
   {
     image_desc: "image 1 for experience 3",
     belongs_to_title: "experience 3",
-    references_image: "",
+    image_URL: "",
   },
   {
     image_desc: "image 1 for experience 4",
     belongs_to_title: "experience 4",
-    references_image: "",
+    image_URL: "",
   },
   {
     image_desc: "image 1 for experience 5",
     belongs_to_title: "experience 5",
-    references_image: "",
+    image_URL: "",
   },
 ];
