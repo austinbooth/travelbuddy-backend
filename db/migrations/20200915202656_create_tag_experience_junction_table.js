5df01a01ed00e0f3f0592e4da4366850491147a3
Austin Booth <austinbooth@yahoo.co.uk>
2020-09-15 21:02:07 +0100

    creates junction table

---
+++ db/migrations/20200915202656_create_tag_experience_junction_table.js
@@ -0,0 +1,14 @@
+exports.up = function (knex) {
+  return knex.schema.createTable("tag_experience_junction", (junctionTable) => {
+    junctionTable.integer("tag_id").references("tags.tag_id").notNullable();
+    junctionTable
+      .integer("experience_id")
+      .references("experiences.experience_id")
+      .notNullable();
+    junctionTable.primary(["tag_id", "experience_id"]);
+  });
+};
+
+exports.down = function (knex) {
+  return knex.schema.dropTable("tag_experience_junction");
+};
