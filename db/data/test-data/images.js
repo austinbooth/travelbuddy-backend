8a837b1e95c24264017255e86f2d6198680b6033
Austin Booth <austinbooth@yahoo.co.uk>
2020-09-15 13:07:24 +0100

    references_image added

--- db/data/test-data/images.js
+++ db/data/test-data/images.js
@@ -2,26 +2,26 @@ module.exports = [
   {
     image_desc: "image 1 for experience 1",
     belongs_to_title: "experience 1",
-    image_url: "",
+    references_image: "",
   },
   {
     image_desc: "image 1 for experience 2",
     belongs_to_title: "experience 2",
-    image_url: "",
+    references_image: "",
   },
   {
     image_desc: "image 1 for experience 3",
     belongs_to_title: "experience 3",
-    image_url: "",
+    references_image: "",
   },
   {
     image_desc: "image 1 for experience 4",
     belongs_to_title: "experience 4",
-    image_url: "",
+    references_image: "",
   },
   {
     image_desc: "image 1 for experience 5",
     belongs_to_title: "experience 5",
-    image_url: "",
+    references_image: "",
   },
 ];
