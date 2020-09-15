67931d1a1eef5256654d212eb299c44fee5dd303
Austin Booth <austinbooth@yahoo.co.uk>
2020-09-15 14:42:48 +0100

    exports data

---
+++ db/data/test-data/index.js
@@ -0,0 +1,5 @@
+exports.commentData = require("./comments");
+exports.experienceData = require("./experiences");
+exports.imageData = require("./images");
+exports.tagData = require("./tags");
+exports.userData = require("./user");
