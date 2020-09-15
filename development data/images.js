7f80997a7cf4ee09036289cc0a99ce45afe2d1d5
helenjanei <helenjanei@gmail.com>
2020-09-15 10:16:41 +0100

    adds initial development data for website

---
+++ development data/images.js
@@ -0,0 +1,69 @@
+module.exports = [
+  {
+    image_desc: "man juggling kittens",
+    belongs_to_title: "Fab place to watch street performers",
+  },
+  {
+    image_desc: "close up of beads on a stall",
+    belongs_to_title: "Really cute craft market.",
+  },
+  {
+    image_desc: "front entrance of the Yorvik centre",
+    belongs_to_title: "Free history lesson outside the Yorvik centre.",
+  },
+  {
+    image_desc: "girl dancing wearing headphones",
+    belongs_to_title: "Murder at the silent disco!",
+  },
+  {
+    image_desc: "two girls pose in front of a flower bed",
+    belongs_to_title: "Great place for photographs",
+  },
+  {
+    image_desc: "family sits around a camp fire at night",
+    belongs_to_title: "Free outdoor cooking.",
+  },
+  {
+    image_desc: "cat sitting outside York Minster",
+    belongs_to_title: "Cat heaven!",
+  },
+  {
+    image_desc: "man and child stand holding spray paint cans",
+    belongs_to_title: "Graffiti workshop",
+  },
+  {
+    image_desc: "spray painted picture of a name",
+    belongs_to_title: "Graffiti workshop",
+  },
+
+  //PARIS
+
+  {
+    image_desc: "Pont de Bir-hakeim at night",
+    belongs_to_title: "WAY COOL BRIDGE",
+  },
+  {
+    image_desc: "a couple share a picnic on a bench in the park",
+    belongs_to_title: "Best place for a picnic",
+  },
+  {
+    image_desc: "a bottle of wine sits on a barrel next to two wine glasses",
+    belongs_to_title: "wine tasting in an underground cellar",
+  },
+  {
+    image_desc: "view across a grave yard with tombs and statues",
+    belongs_to_title: "hang out with the dead in style",
+  },
+  {
+    image_desc: "outside of the Pompidou Centre",
+    belongs_to_title: "people watching in style",
+  },
+  {
+    image_desc: "brightly coloured mural on the side of a bilding",
+    belongs_to_title: "cool street art",
+  },
+  {
+    image_desc: "cafe table on balcony at the Louvre Museum",
+    belongs_to_title: "luxury lunch like a Queen",
+  },
+];
