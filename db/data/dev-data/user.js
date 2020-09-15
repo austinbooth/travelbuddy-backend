96b14afcabb0d09dc35481b33f49d3e5912fd851
helenjanei <helenjanei@gmail.com>
2020-09-15 10:57:16 +0100

    creates a dictionary of usernames  to prevent spelling errors being flaggedl

--- db/data/dev-data/user.js
+++ db/data/dev-data/user.js
@@ -13,35 +13,56 @@ module.exports = [{
   },
   {
     username: 'tillRitzy',
-     last_name: 'Small', first_name: 'Izabelle', 
-     avatar_URL:'https://avatars.dicebear.com/v2/female/5d6963b3dbe5b81ef98335ba1bed67c9.svg'
-  }, 
+    last_name: 'Small',
+    first_name: 'Izabelle',
+    avatar_URL: 'https://avatars.dicebear.com/v2/female/5d6963b3dbe5b81ef98335ba1bed67c9.svg'
+  },
   {
     username: 'hartHouseCampbellton',
-     last_name: 'Baxter',  first_name: 'Sameer'.  avatar_URL:'https://api.adorable.io/avatars/400/f16cd5431d99eaf7a9d16b2de96d5f82.png'
+    last_name: 'Baxter',
+    first_name: 'Sameer',
+    avatar_URL: 'https://api.adorable.io/avatars/400/f16cd5431d99eaf7a9d16b2de96d5f82.png'
   },
- {
+  {
     username: 'bookTrain',
-     last_name: 'Stanley', first_name: 'Cassia', 
-     avatar_URL:'https://robohash.org/f16cd5431d99eaf7a9d16b2de96d5f82?set=set4&bgset=&size=400x400'
-  }, {
-    username:'tableSpoonSurprise',
-     last_name:  'Copeland', first_name: 'Ruairi', avatar_URL:'https://api.adorable.io/avatars/400/0abaae2007f0911e6680edf096a9477d.png'
-  },{
-    username:'rosiebattery',
-     last_name: 'Preston', first_name: 'Skylar', emailavatar_URL:'https://avatars.dicebear.com/v2/female/0abaae2007f0911e6680edf096a9477d.svg'
-  },{
-    username:'luggTattling',
-     last_name: 'Greenwood',
-     first_name: 'Primrose',  avatar_URL:'https://robohash.org/0abaae2007f0911e6680edf096a9477d?set=set4&bgset=&size=400x400'
-  },{
-    username:'rumblingCSharp',
-     last_name:  'Jensen',
-     first_name: 'Iqrah',avatar_URL: 'https://avatars.dicebear.com/v2/female/0abaae2007f0911e6680edf096a9477d.svg'},{
-    username:'nelsonHilarious',
-     last_name: 'Hendrix',
-     first_name:  'Ryker', avatar_URL: 'https://robohash.org/6e091e09e2662b74ac18d288ff948e0f?set=set4&bgset=&size=400x400}',{
+    last_name: 'Stanley',
+    first_name: 'Cassia',
+    avatar_URL: 'https://robohash.org/f16cd5431d99eaf7a9d16b2de96d5f82?set=set4&bgset=&size=400x400'
+  },
+  {
+    username: 'tableSpoonSurprise',
+    last_name: 'Copeland',
+    first_name: 'Ruairi',
+    avatar_URL: 'https://api.adorable.io/avatars/400/0abaae2007f0911e6680edf096a9477d.png'
+  },
+  {
+    username: 'rosiebattery',
+    last_name: 'Preston',
+    first_name: 'Skylar',
+    avatar_URL: 'https://avatars.dicebear.com/v2/female/0abaae2007f0911e6680edf096a9477d.svg'
+  },
+  {
+    username: 'luggTattling',
+    last_name: 'Greenwood',
+    first_name: 'Primrose',
+    avatar_URL: 'https://robohash.org/0abaae2007f0911e6680edf096a9477d?set=set4&bgset=&size=400x400'
+  },
+  {
+    username: 'rumblingCSharp',
+    last_name: 'Jensen',
+    first_name: 'Iqrah',
+    avatar_URL: 'https://avatars.dicebear.com/v2/female/0abaae2007f0911e6680edf096a9477d.svg'
+  },
+  {
+    username: 'nelsonHilarious',
+    last_name: 'Hendrix',
+    first_name: 'Ryker',
+    avatar_URL: 'https://robohash.org/6e091e09e2662b74ac18d288ff948e0f?set=set4&bgset=&size=400x400'
+  },
+  {
     username: 'debatablePassport',
-     last_name:  'Benton',
-     first_name: 'Manav',avatar_URL: 'https://avatars.dicebear.com/v2/female/5f86d198f6fc959328a286d387221b2a.svg}'
+    last_name: 'Benton',
+    first_name: 'Manav',
+    avatar_URL: 'https://avatars.dicebear.com/v2/female/5f86d198f6fc959328a286d387221b2a.svg'
+  }
 ]
