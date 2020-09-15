625043d75a51c3a1d5b32eee8a0f83e822a22330
Austin Booth <austinbooth@yahoo.co.uk>
2020-09-15 16:59:20 +0100

    created to allow junction tabel to be created during seeding

---
+++ db/data/dev-data/tag-experience-junctions.js
@@ -0,0 +1,176 @@
+module.exports = [
+  {
+    tag_text: "#streetPerformers",
+    belongs_to_experience_title: "Fab place to watch street performers.",
+  },
+  {
+    tag_text: "#outdoorExperience",
+    belongs_to_experience_title: "Fab place to watch street performers.",
+  },
+  {
+    tag_text: "#realCraft",
+    belongs_to_experience_title: "Really cute craft market.",
+  },
+  {
+    tag_text: "#kidsExperience",
+    belongs_to_experience_title: "Really cute craft market.",
+  },
+  {
+    tag_text: "#uniqueGifts",
+    belongs_to_experience_title: "Really cute craft market.",
+  },
+  {
+    tag_text: "#kidsExperience",
+    belongs_to_experience_title:
+      "Free history lesson outside the Yorvik centre.",
+  },
+  {
+    tag_text: "#encounterWithHistory",
+    belongs_to_experience_title:
+      "Free history lesson outside the Yorvik centre.",
+  },
+  {
+    tag_text: "#kidsExperience",
+    belongs_to_experience_title: "Murder at the silent disco!",
+  },
+  {
+    tag_text: "#unusualEntertainment",
+    belongs_to_experience_title: "Murder at the silent disco!",
+  },
+  {
+    tag_text: "#dance",
+    belongs_to_experience_title: "Murder at the silent disco!",
+  },
+  {
+    tag_text: "#music",
+    belongs_to_experience_title: "Murder at the silent disco!",
+  },
+  {
+    tag_text: "#natureIsBeautiful",
+    belongs_to_experience_title: "Great place for photographs.",
+  },
+  {
+    tag_text: "#photography",
+    belongs_to_experience_title: "Great place for photographs.",
+  },
+  {
+    tag_text: "#outdoorExperience",
+    belongs_to_experience_title: "Great place for photographs.",
+  },
+  {
+    tag_text: "#natureIsBeautiful",
+    belongs_to_experience_title: "Free outdoor cooking.",
+  },
+  {
+    tag_text: "#outdoorExperience",
+    belongs_to_experience_title: "Free outdoor cooking.",
+  },
+  {
+    tag_text: "#outdoorExperience",
+    belongs_to_experience_title: "Cat heaven!",
+  },
+  {
+    tag_text: "#communityLife",
+    belongs_to_experience_title: "Cat heaven!",
+  },
+  {
+    tag_text: "#outdoorExperience",
+    belongs_to_experience_title: "Running + Dancing = Rancing!",
+  },
+  {
+    tag_text: "#dance",
+    belongs_to_experience_title: "Running + Dancing = Rancing!",
+  },
+  {
+    tag_text: "#ART",
+    belongs_to_experience_title: "Graffiti workshop",
+  },
+  {
+    tag_text: "#kidsExperience",
+    belongs_to_experience_title: "Graffiti workshop",
+  },
+  {
+    tag_text: "#YOLO",
+    belongs_to_experience_title: "Had my palm read by a psychic!",
+  },
+  {
+    tag_text: "#amazingArchitecture",
+    belongs_to_experience_title: "WAY COOL BRIDGE",
+  },
+  {
+    tag_text: "#photography",
+    belongs_to_experience_title: "WAY COOL BRIDGE",
+  },
+  {
+    tag_text: "#amazingArchitecture",
+    belongs_to_experience_title: "Best place for a picnic",
+  },
+  {
+    tag_text: "#cheapEats",
+    belongs_to_experience_title: "Best place for a picnic",
+  },
+  {
+    tag_text: "#photography",
+    belongs_to_experience_title: "Best place for a picnic",
+  },
+  {
+    tag_text: "#YOLO",
+    belongs_to_experience_title: "hang out with the dead in style",
+  },
+  {
+    tag_text: "#outdoorExperienc",
+    belongs_to_experience_title: "hang out with the dead in style",
+  },
+  {
+    tag_text: "#unusualEntertainment",
+    belongs_to_experience_title: "hang out with the dead in style",
+  },
+  {
+    tag_text: "#YOLO",
+    belongs_to_experience_title: "vintage bargains - great window shopping!",
+  },
+  {
+    tag_text: "#drinks",
+    belongs_to_experience_title: "wine tasting in an underground cellar",
+  },
+  {
+    tag_text: "#authenticExperience",
+    belongs_to_experience_title: "wine tasting in an underground cellar",
+  },
+  {
+    tag_text: "#authenticExperience",
+    belongs_to_experience_title: "people watching in style",
+  },
+  {
+    tag_text: "#photography",
+    belongs_to_experience_title: "people watching in style",
+  },
+  {
+    tag_text: "#ART",
+    belongs_to_experience_title: "people watching in style",
+  },
+  {
+    tag_text: "#streetPerformers",
+    belongs_to_experience_title: "people watching in style",
+  },
+  {
+    tag_text: "#music",
+    belongs_to_experience_title: "people watching in style",
+  },
+  {
+    tag_text: "#ART",
+    belongs_to_experience_title: "cool street art",
+  },
+  {
+    tag_text: "#authenticExperience",
+    belongs_to_experience_title: "real person shopping in Paris",
+  },
+  {
+    tag_text: "#uniqueGifts",
+    belongs_to_experience_title: "real person shopping in Paris",
+  },
+  {
+    tag_text: "#YOLO",
+    belongs_to_experience_title: "luxury lunch like a Queen",
+  },
+];
