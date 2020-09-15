20a87af79a8dc8de35566e8f04a8f93a8585a801
Austin Booth <austinbooth@yahoo.co.uk>
2020-09-15 10:24:37 +0100

    created_at datatype changed from string to number

--- db/data/test-data/comments.js
+++ db/data/test-data/comments.js
@@ -1,13 +1,13 @@
 module.exports = [
   {
-    created_at: "1586549847000",
+    created_at: 1586549847000,
     body: "body for comment 1 for experience 1",
     likes: "1",
     username: "user_b",
     belongs_to_title: "experience 1",
   },
   {
-    created_at: "1594549847000",
+    created_at: 1594549847000,
     body: "body for comment 2 for experience 1",
     likes: "5",
     username: "user_c",
@@ -15,7 +15,7 @@ module.exports = [
   },
 
   {
-    created_at: "1574549847000",
+    created_at: 1574549847000,
     body: "body for comment 1 for experience 2",
     likes: "1",
     username: "user_a",
@@ -23,14 +23,14 @@ module.exports = [
   },
 
   {
-    created_at: "1578858463000",
+    created_at: 1578858463000,
     body: "body for comment 1 for experience 3",
     likes: "1",
     username: "user_a",
     belongs_to_title: "experience 1",
   },
   {
-    created_at: "1582858463000",
+    created_at: 1582858463000,
     body: "body for comment 2 for experience 3",
     likes: "5",
     username: "user_b",
@@ -38,7 +38,7 @@ module.exports = [
   },
 
   {
-    created_at: "1576889475000",
+    created_at: 1576889475000,
     body: "body for comment 1 for experience 4",
     likes: "1",
     username: "user_b",
@@ -46,14 +46,14 @@ module.exports = [
   },
 
   {
-    created_at: "1586866595000",
+    created_at: 1586866595000,
     body: "body for comment 1 for experience 5",
     likes: "1",
     username: "user_b",
     belongs_to_title: "experience 4",
   },
   {
-    created_at: "1589886795000",
+    created_at: 1589886795000,
     body: "body for comment 1 for experience 5",
     likes: "1",
     username: "user_b",
