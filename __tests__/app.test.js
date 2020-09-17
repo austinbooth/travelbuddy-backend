b3392cf761254454f18da3e993998919bd9aa591
helenjanei <helenjanei@gmail.com>
2020-09-17 15:31:40 +0100

    adds testing for 'experiences' end point

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,54 @@ const request = require("supertest");
 
 describe("app", () => {
   describe("/graphql", () => {
-    test("POST: 200 - responds with JSON for all experiences", () => {
-      const query = { query: "{experiences {experience_id}}" };
-      return request(app)
-        .post("/graphql")
-        .send(query)
-        .expect(200)
-        .then(({ body: { data } }) => console.log(data));
+    describe("/graphql", () => {
+      test("POST: 200 - responds with JSON for all experiences", () => {
+        const query = {
+          query: "{experiences {title}}"
+        };
+        return request(app)
+          .post("/graphql")
+          .send(query)
+          .expect(200)
+          .then(({
+            body: {
+              data
+            }
+          }) => {
+            expect(Array.isArray(data.experiences)).toBe(true);
+            expect(data.experiences.length).toBe(5);
+
+          });
+      });
+      test("POST: 200 - responds with JSON containing all key value pairs when passed all keys", () => {
+        const query = {
+          query: "{experiences { experience_id,title,  body, username, created_at,location_lat,  location_long,likes }}"
+        };
+        return request(app)
+          .post("/graphql")
+          .send(query)
+          .expect(200)
+          .then(({
+            body: {
+              data
+            }
+          }) => {
+            expect(data.experiences[0]).toEqual(
+              expect.objectContaining({
+                title: "experience 1",
+                body: "body for experience 1",
+                username: "user_a",
+                created_at: "1584549747000",
+                experience_id: "1",
+                location_lat: "53.955609",
+                location_long: "-1.078392",
+                likes: 5,
+
+              })
+            );
+
+          });
+      });
     });
-  });
+  })
 });
