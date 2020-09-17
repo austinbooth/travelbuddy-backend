9df0f4127205380c6dbc8f0f20e5986979b8f47f
helenjanei <helenjanei@gmail.com>
2020-09-17 12:08:33 +0100

    adds tagsFOrAnExperience to schema

--- schema.js
+++ schema.js
@@ -11,45 +11,87 @@ const {
 const ExperienceType = new GraphQLObjectType({
   name: "Experiences",
   fields: () => ({
-    experience_id: { type: GraphQLID },
-    title: { type: GraphQLString },
-    body: { type: GraphQLString },
-    username: { type: GraphQLString },
-    created_at: { type: GraphQLString },
-    location_lat: { type: GraphQLString },
-    location_long: { type: GraphQLString },
-    likes: { type: GraphQLInt },
+    experience_id: {
+      type: GraphQLID
+    },
+    title: {
+      type: GraphQLString
+    },
+    body: {
+      type: GraphQLString
+    },
+    username: {
+      type: GraphQLString
+    },
+    created_at: {
+      type: GraphQLString
+    },
+    location_lat: {
+      type: GraphQLString
+    },
+    location_long: {
+      type: GraphQLString
+    },
+    likes: {
+      type: GraphQLInt
+    },
   }),
 });
 
 const CommentType = new GraphQLObjectType({
   name: "Comments",
   fields: () => ({
-    comment_id: { type: GraphQLID },
-    created_at: { type: GraphQLString },
-    body: { type: GraphQLString },
-    likes: { type: GraphQLInt },
-    username: { type: GraphQLString },
-    experience_id: { type: GraphQLID },
+    comment_id: {
+      type: GraphQLID
+    },
+    created_at: {
+      type: GraphQLString
+    },
+    body: {
+      type: GraphQLString
+    },
+    likes: {
+      type: GraphQLInt
+    },
+    username: {
+      type: GraphQLString
+    },
+    experience_id: {
+      type: GraphQLID
+    },
   }),
 });
 
 const ImageType = new GraphQLObjectType({
   name: "Images",
   fields: () => ({
-    image_id: { type: GraphQLID },
-    image_desc: { type: GraphQLString },
-    experience_id: { type: GraphQLID },
-    image_URL: { type: GraphQLString },
+    image_id: {
+      type: GraphQLID
+    },
+    image_desc: {
+      type: GraphQLString
+    },
+    experience_id: {
+      type: GraphQLID
+    },
+    image_URL: {
+      type: GraphQLString
+    },
   }),
 });
 
 const TagType = new GraphQLObjectType({
   name: "TagsForAnExperience",
   fields: () => ({
-    tag_id: { type: GraphQLID },
-    tag_text: { type: GraphQLString },
-    experience_id: { type: GraphQLID },
+    tag_id: {
+      type: GraphQLID
+    },
+    tag_text: {
+      type: GraphQLString
+    },
+    experience_id: {
+      type: GraphQLID
+    },
   }),
 });
 
@@ -58,9 +100,15 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     experience: {
       type: ExperienceType,
-      args: { experience_id: { type: GraphQLID } },
+      args: {
+        experience_id: {
+          type: GraphQLID
+        }
+      },
       resolve(parent, args) {
-        const { experience_id } = args;
+        const {
+          experience_id
+        } = args;
         return db("experiences")
           .where("experience_id", experience_id)
           .then(([experience]) => experience);
@@ -74,9 +122,15 @@ const RootQuery = new GraphQLObjectType({
     },
     comments: {
       type: new GraphQLList(CommentType),
-      args: { experience_id: { type: GraphQLID } },
+      args: {
+        experience_id: {
+          type: GraphQLID
+        }
+      },
       resolve(parent, args) {
-        const { experience_id } = args;
+        const {
+          experience_id
+        } = args;
         return db("comments")
           .where("experience_id", experience_id)
           .then((comments) => comments);
@@ -84,18 +138,32 @@ const RootQuery = new GraphQLObjectType({
     },
     images: {
       type: new GraphQLList(ImageType),
-      args: { experience_id: { type: GraphQLID } },
+      args: {
+        experience_id: {
+          type: GraphQLID
+        }
+      },
       resolve(parent, args) {
-        const { experience_id } = args;
+        const {
+          experience_id
+        } = args;
         return db("images").where("experience_id", experience_id);
       },
     },
     tagsForAnExperience: {
       type: new GraphQLList(TagType),
-      args: { experience_id: { type: GraphQLID } },
+      args: {
+        experience_id: {
+          type: GraphQLID
+        }
+
+      },
       resolve(parent, args) {
-        const { experience_id } = args;
-        // return db("tags").innerJoin("tag_experience_junction", experience_id);
+        const {
+          experience_id
+        } = args;
+        return db.select('tags.tag_id', 'tags.tag_text', 'tag_experience_junction.experience_id').from("tags").innerJoin("tag_experience_junction", "tags.tag_id", "tag_experience_junction.tag_id").where("tag_experience_junction.experience_id",
+          experience_id)
       },
     },
   },
