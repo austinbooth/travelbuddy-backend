625357b15bc4c6616900478722d725a274748138
Rosalyn Land <roz.s.land@gmail.com>
2020-09-21 09:26:19 +0100

    exports types to a separate file

--- schema.js
+++ schema.js
@@ -2,11 +2,12 @@ const db = require("./db/connection");
 const {
   GraphQLSchema,
   GraphQLObjectType,
-  GraphQLString,
   GraphQLID,
-  GraphQLInt,
   GraphQLList,
 } = require("graphql");
+
+const { ExperienceType, CommentType, ImageType, TagType } = require("./types");
+
 const {
   InputExperienceType,
   InputCommentType,
@@ -14,93 +15,6 @@ const {
   InputUpdateExperienceType,
   InputUpdateExperienceLikesType,
 } = require("./inputTypes");
-
-const ExperienceType = new GraphQLObjectType({
-  name: "Experiences",
-  fields: () => ({
-    experience_id: {
-      type: GraphQLID,
-    },
-    title: {
-      type: GraphQLString,
-    },
-    body: {
-      type: GraphQLString,
-    },
-    username: {
-      type: GraphQLString,
-    },
-    created_at: {
-      type: GraphQLString,
-    },
-    location_lat: {
-      type: GraphQLString,
-    },
-    location_long: {
-      type: GraphQLString,
-    },
-    likes: {
-      type: GraphQLInt,
-    },
-  }),
-});
-
-const CommentType = new GraphQLObjectType({
-  name: "Comments",
-  fields: () => ({
-    comment_id: {
-      type: GraphQLID,
-    },
-    created_at: {
-      type: GraphQLString,
-    },
-    body: {
-      type: GraphQLString,
-    },
-    likes: {
-      type: GraphQLInt,
-    },
-    username: {
-      type: GraphQLString,
-    },
-    experience_id: {
-      type: GraphQLID,
-    },
-  }),
-});
-
-const ImageType = new GraphQLObjectType({
-  name: "Images",
-  fields: () => ({
-    image_id: {
-      type: GraphQLID,
-    },
-    image_desc: {
-      type: GraphQLString,
-    },
-    experience_id: {
-      type: GraphQLID,
-    },
-    image_URL: {
-      type: GraphQLString,
-    },
-  }),
-});
-
-const TagType = new GraphQLObjectType({
-  name: "TagsForAnExperience",
-  fields: () => ({
-    tag_id: {
-      type: GraphQLID,
-    },
-    tag_text: {
-      type: GraphQLString,
-    },
-    experience_id: {
-      type: GraphQLID,
-    },
-  }),
-});
 
 const RootQuery = new GraphQLObjectType({
   name: "RootQueryType",
---
+++ types.js
@@ -0,0 +1,93 @@
+const {
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLInt,
+} = require("graphql");
+
+exports.ExperienceType = new GraphQLObjectType({
+  name: "Experiences",
+  fields: () => ({
+    experience_id: {
+      type: GraphQLID,
+    },
+    title: {
+      type: GraphQLString,
+    },
+    body: {
+      type: GraphQLString,
+    },
+    username: {
+      type: GraphQLString,
+    },
+    created_at: {
+      type: GraphQLString,
+    },
+    location_lat: {
+      type: GraphQLString,
+    },
+    location_long: {
+      type: GraphQLString,
+    },
+    likes: {
+      type: GraphQLInt,
+    },
+  }),
+});
+
+exports.CommentType = new GraphQLObjectType({
+  name: "Comments",
+  fields: () => ({
+    comment_id: {
+      type: GraphQLID,
+    },
+    created_at: {
+      type: GraphQLString,
+    },
+    body: {
+      type: GraphQLString,
+    },
+    likes: {
+      type: GraphQLInt,
+    },
+    username: {
+      type: GraphQLString,
+    },
+    experience_id: {
+      type: GraphQLID,
+    },
+  }),
+});
+
+exports.ImageType = new GraphQLObjectType({
+  name: "Images",
+  fields: () => ({
+    image_id: {
+      type: GraphQLID,
+    },
+    image_desc: {
+      type: GraphQLString,
+    },
+    experience_id: {
+      type: GraphQLID,
+    },
+    image_URL: {
+      type: GraphQLString,
+    },
+  }),
+});
+
+exports.TagType = new GraphQLObjectType({
+  name: "TagsForAnExperience",
+  fields: () => ({
+    tag_id: {
+      type: GraphQLID,
+    },
+    tag_text: {
+      type: GraphQLString,
+    },
+    experience_id: {
+      type: GraphQLID,
+    },
+  }),
+});
